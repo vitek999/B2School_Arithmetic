fdd11b061ef7b42bd29a6246f45b7c1fd2eb96d8
vitek999 <vitek2012rus@gmail.com>
2020-05-03 15:31:57 +0400

    Added bottom navigation graph

--- app/src/main/java/com/test/b2schoolarithmetic/MainActivity.kt
+++ app/src/main/java/com/test/b2schoolarithmetic/MainActivity.kt
@@ -2,6 +2,9 @@ package com.test.b2schoolarithmetic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        val destinationsWithHideKeyboard = listOf(R.id.loginFragment)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            bottom_navigation.visibility = if(destination.id in destinationsWithHideKeyboard) View.GONE else View.VISIBLE
+        }
     }
 }
